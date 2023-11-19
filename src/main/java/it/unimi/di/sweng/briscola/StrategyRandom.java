1420cd4c9d67963a840539a044a7260501bc3570
michelebolis <mickbolis@gmail.com>
2023-11-19 16:15:52 +0100

    VERDE: randomStrategyTest passed

--- src/main/java/it/unimi/di/sweng/briscola/StrategyRandom.java
+++ src/main/java/it/unimi/di/sweng/briscola/StrategyRandom.java
@@ -2,9 +2,15 @@ package it.unimi.di.sweng.briscola;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class StrategyRandom implements Strategy{
     @Override
     public @NotNull Card chooseCard(@NotNull Player me, @NotNull Player other, @NotNull Suit briscola) {
-        return null;
+        ArrayList<Card> cards = new ArrayList<>();
+        for (Card card:me) { cards.add(card); }
+        Collections.shuffle(cards);
+        return cards.get(0);
     }
 }
