26b4d686f71af16e465bdde1896385865a21b2d3
michelebolis <mickbolis@gmail.com>
2023-11-19 15:04:47 +0100

    ROSSO: establishTurnWinnerSameSeedTest failed

---
+++ src/test/java/it/unimi/di/sweng/briscola/BriscolaTest.java
@@ -0,0 +1,25 @@
+package it.unimi.di.sweng.briscola;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class BriscolaTest {
+    @Test
+    public void establishTurnWinnerSameSeedTest(){
+        Player p1 = mock(Player.class);
+        Player p2 = mock(Player.class);
+        Deck deck = mock(Deck.class);
+        Card card = Card.get(Rank.CINQUE, Suit.BASTONI);
+        Card briscola = Card.get(Rank.CINQUE, Suit.COPPE);
+        when(deck.draw()).thenReturn(card, card, card, card, card, card, briscola);
+        Briscola partita = new Briscola(p1, p2, deck);
+        Card cardVincente = Card.get(Rank.ASSO, Suit.COPPE);
+        Card cardPerdente = Card.get(Rank.TRE, Suit.COPPE);
+        assertThat(Optional.of(partita.establishTurnWinner(cardVincente, cardPerdente))).isEqualTo(Optional.of(p1));
+    }
+}
