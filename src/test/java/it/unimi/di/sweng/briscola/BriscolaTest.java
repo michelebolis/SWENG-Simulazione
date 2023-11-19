6db42e0fd08d1509acb17961b8ea3d021568b244
michelebolis <mickbolis@gmail.com>
2023-11-19 15:21:47 +0100

    ROSSO: establishTurnWinnerP2BriscolaTest failed

--- src/test/java/it/unimi/di/sweng/briscola/BriscolaTest.java
+++ src/test/java/it/unimi/di/sweng/briscola/BriscolaTest.java
@@ -35,4 +35,17 @@ public class BriscolaTest {
         Card cardPerdente = Card.get(Rank.TRE, Suit.SPADE);
         assertThat(Optional.of(partita.establishTurnWinner(cardVincente, cardPerdente))).isEqualTo(Optional.of(p1));
     }
+    @Test
+    public void establishTurnWinnerP2BriscolaTest(){
+        Player p1 = mock(Player.class);
+        Player p2 = mock(Player.class);
+        Deck deck = mock(Deck.class);
+        Card card = Card.get(Rank.CINQUE, Suit.BASTONI);
+        Card briscola = Card.get(Rank.CINQUE, Suit.COPPE);
+        when(deck.draw()).thenReturn(card, card, card, card, card, card, briscola);
+        Briscola partita = new Briscola(p1, p2, deck);
+        Card cardVincente = Card.get(Rank.ASSO, Suit.SPADE);
+        Card cardPerdente = Card.get(Rank.DUE, Suit.COPPE);
+        assertThat(Optional.of(partita.establishTurnWinner(cardVincente, cardPerdente))).isEqualTo(Optional.of(p2));
+    }
 }
